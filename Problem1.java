aab69c748b003615b921b8f8425c4891e9a33fc6
Keerthi0910 <chundurikeerthipriya@gmail.com>
2026-09-23 14:30:38 -0500

    Add word break solution using dynamic programming
    Implemented dynamic programming solution for word break problem.

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,24 @@
+// dp with space complexity as O(n)
+// time complexity for both for loops is o(n) and for string opwration its o(n) alltogether o(n3) time complexity
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        HashSet<String> set = new HashSet<>();
 
+        for(String word: wordDict){
+            set.add(word);
+        }
+        boolean[] dp = new boolean[n +1];
+        dp[0] = true;
+        for(int i = 1 ; i <= n ; i++){
+            for (int j = 0 ; j< i ; j++){
+                if(dp[j] && set.contains(s.substring(j, i))){
+                    dp[i] = true;
+                    break;
+
+                }
+             }
+        }
+        return dp[n];
+    }
+}
